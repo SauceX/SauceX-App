bdf99f10ca71435fb6ef7cb398cea72dcd5e8d4b
yinzhennan <yinzhennan123@gmial.com>
2018-09-10 12:16:10 +0800

    上传

--- src/app/cook/cook.component.ts
+++ src/app/cook/cook.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as ons from 'onsenui';
+import { Http } from '@angular/http';
+import { Headers, RequestOptions, URLSearchParams } from '@angular/http';
 
 @Component({
   selector: 'app-cook',
@@ -14,10 +16,19 @@ export class CookComponent implements OnInit {
   }
 
   toastStart() {
+    const data = new URLSearchParams();
+    const bodyData = {
+      'UUID': 'nullfy6HS'
+    };
+    data.append('body', JSON.stringify(bodyData));
+    data.append('tradeCode', '0002');
+    data.append('tradeType', 'authService');
+    this.http.post('', data, this.options);
+
     ons.notification.toast('已同步至终端,在臻美味智能酱料罐上按开始键后即出蘸料!', {timeout: 5000});
   }
 
   toastSync() {
-    ons.notification.toast('已同步至终端!', {timeout: 2000});
+    ons.notification.toast('搜藏成功!', {timeout: 2000});
   }
 }
